6e08ba1a36d9ce691bb66c3a536834209e04f562
Scott Hilbert <github@shilbert.com>
2012-10-20 11:12:21 -0400

    BlendFunction.{Min,Max} ought to be available on any non-GLES target, not just MonoMac

--- MonoGame.Framework/Graphics/GraphicsExtensions.cs
+++ MonoGame.Framework/Graphics/GraphicsExtensions.cs
@@ -350,7 +350,7 @@ namespace Microsoft.Xna.Framework.Graphics
 				return BlendEquationMode.MaxExt;
 			case BlendFunction.Min:
 				return BlendEquationMode.MinExt;
-#elif MONOMAC
+#elif !GLES
 			case BlendFunction.Max:
 				return BlendEquationMode.Max;
 			case BlendFunction.Min:
